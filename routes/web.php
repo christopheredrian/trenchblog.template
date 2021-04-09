93586e5e542f218d3a47b6b7cc778d74128f6f3f
Christopher Espiritu <christopheredrian@gmail.com>
2021-04-08 22:00:24 -0400

    test change

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ use Illuminate\Support\Facades\Route;
 //Route::get('/', function () {
 //    return view('welcome');
 //});
-
+//
 Route::prefix('canvas-ui')->group(function () {
     Route::prefix('api')->group(function () {
         Route::get('posts', [CanvasUiController::class, 'getPosts']);
