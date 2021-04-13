f715c22a44b6fb81b874895f1b424e01db3e3889
Christopher Espiritu <christopheredrian@gmail.com>
2021-04-13 00:10:50 -0400

    Update about.blade.php

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -5,12 +5,32 @@
     <h2 class="border-bottom">About Me</h2>
 
     <div class="mt-4 p-2">
+
         <p>
-            My name is Donmari. I am happily married to Chris, and we have a 10 month old son, Christopher, but we call him by his nickname, Bokchoy. Interestingly enough, ‘Growing Bokchoy’ fits perfectly as we will see him grow throughout my blog posts.
+            My name is Donmari, and I’m a first-time and stay-at-home mom. I’m happily married to my longtime high
+            school
+            crush, Chris.
         </p>
 
         <p>
-            My blog is my open journal. I am sharing my personal struggle as a mom, and I want you to be as open minded as possible. We are all different in raising our children. I am raising my son this way, and I am very open in taking advice and learning from you. I want my blog to be a safe space to other moms, struggling or not. We have different battles we’re taking, so please bear with me as I go with you in this journey.
+            We have a 10 month old son, Christopher, but we call him by his nickname, Bokchoy. Interestingly enough,
+            ‘Growing Bokchoy’ fits perfectly for the blog name as we will see him grow throughout my blog posts.
         </p>
+
+        <p>
+            Before I gave birth, no matter how much I read or researched about motherhood and parenting, I had to learn
+            the
+            hard way of knowing that every child is different and nothing is consistent. I also learned that some of my
+            experiences could be the same as someone else’s. And that is why I wanted to share my experience and
+            struggle as
+            a mom.
+        </p>
+
+        <p>
+            I want my blog to be a safe space for other moms. We have different battles we’re taking, so please bear
+            with me
+            as I go with you on this journey.
+        </p>
+
     </div>
 @endsection
