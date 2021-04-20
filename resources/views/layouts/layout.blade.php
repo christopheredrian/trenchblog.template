4741e23a9302482dc2ad2cd5ee3d7ee7e353a0c6
Christopher Espiritu <christopheredrian@gmail.com>
2021-04-19 20:56:15 -0400

    Update layout.blade.php

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -8,6 +8,7 @@
 
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="mobile-web-app-capable" content="yes">
 
     <style>
 
